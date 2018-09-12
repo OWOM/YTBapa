0e5860a8f59886cc5ac455527d9448fc6b245ceb
OWOM <37099453+OWOM@users.noreply.github.com>
2018-09-12 20:50:32 +0800

    添加了值传递和引用传递

---
+++ src/Bll/YTBapa.Services/Example.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YTBapa.Services
+{
+    public  class Example
+    {
+
+
+        #region 例子1（使用值传递）
+        /// <summary>
+        /// 例子1 算出税后工资
+        /// </summary>
+        /// <param name="pay"></param>
+        /// <param name="taxRate"></param>
+        /// <returns></returns>
+        //public float GetPay(int pay, float taxRate)
+        //{
+        //    float afterTax;
+
+        //    if (pay <= 3500)
+        //    {
+        //        afterTax = pay;
+        //    }
+        //    else
+        //    {
+        //        afterTax = pay - (pay - 3500) * taxRate;
+        //    }
+        //    return afterTax;
+
+        //}
+        //public void Inputpay()
+        //{
+
+        //    int pay; //税前工资
+        //    float taxRate = 0.1f;  //税利
+        //    float afterTax;  //税后工资
+
+        //    Console.WriteLine("请输入税前工资(元)：");
+        //    pay = int.Parse(Console.ReadLine());
+        //    afterTax = GetPay(pay, taxRate);
+        //    Console.WriteLine("税前工资：{0}，税后工资：{1}", pay, afterTax);
+        //} 
+        #endregion
+
+        #region 例子2（使用引用传递）
+        public void reference()
+        {
+            int num1, num2;
+            Console.WriteLine("请输入两个数字：");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("转换前：{0}，{1}", num1, num2);
+            Sawp(ref num1, ref num2);
+            Console.WriteLine("转换后：{0}，{1}", num1, num2);
+
+        }
+        /// <summary>
+        /// 使用引用传递转换数字
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        public void Sawp(ref int num1, ref int num2)
+        {
+            int tem;
+            tem = num1;
+            num1 = num2;
+            num2 = tem;
+        } 
+        #endregion
+
+    }
+
+}
--- src/Tools/YTBapa.PanelTool/Program.cs
+++ src/Tools/YTBapa.PanelTool/Program.cs
@@ -6,16 +6,22 @@ namespace YTbapa.PanelTool
 {
    public class Program
     {
-        static void Main(string[] args)
+        
+       static void Main(string[] args)
         {
-            Bank bank = new Bank();
-            bank.Registered();
-            bank.Options();
+            Example example = new Example();
 
+            //Bank bank = new Bank();
+            //bank.Registered();
+            //bank.Options();
 
+            //例子1
+            //example.Inputpay();
+            //例子2
+            example.reference();
+
+            Console.ReadLine();
         }
-
        
-
     }
 }
